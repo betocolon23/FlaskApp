22d791d5659c12a343969ea6fb27bca7fc2c53e7
Beto Colon <betocolon23@gmail.com>
2017-07-14 14:24:04 -0400

    wtf form

--- __init__.py
+++ __init__.py
@@ -43,15 +43,15 @@ def login_page():
         return render_template("login.html", error = error)
 
 
-# class RegistrationForm(Form):
-#     username = TextField('Username', [validators.Length(min=4, max=20)])
-#     email = TextField('Email Address', [validators.Length(min=6, max=50)])
-#     password = PasswordField('New Password', [
-#         validators.Required(),
-#         validators.EqualTo('confirm', message='Passwords must match')
-#     ])
-#     confirm = PasswordField('Repeat Password')
-#     accept_tos = BooleanField('I accept the Terms of Service and Privacy Notice (updated Jan 22, 2015)', [validators.Required()])
+class RegistrationForm(Form):
+    username = TextField('Username', [validators.Length(min=4, max=20)])
+    email = TextField('Email Address', [validators.Length(min=6, max=50)])
+    password = PasswordField('New Password', [
+        validators.Required(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+    accept_tos = BooleanField('I accept the Terms of Service and Privacy Notice (updated Jan 22, 2015)', [validators.Required()])
 
 @app.route('/register/', methods = ['GET', 'POST'])
 def register_page():

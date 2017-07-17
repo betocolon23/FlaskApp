6a6cb7d27cb5bfc6c4c9ad0c6df5a6d93844c704
Beto Colon <betocolon23@gmail.com>
2017-07-17 13:41:21 -0400

    pruebas

--- __init__.py
+++ __init__.py
@@ -48,8 +48,8 @@ def login_page():
 
 # Clase para hacer la forma de registro
 class RegistrationForm(Form):
-    username = StringField('Username', [validators.Length(min=4, max=20)])
-    email = StringField('Email Address', [validators.Length(min=6, max=50)])
+    username = TextField('Username', [validators.Length(min=4, max=20)])
+    email = TextField('Email Address', [validators.Length(min=6, max=50)])
     password = PasswordField('New Password', [validators.DataRequired(), validators.EqualTo('confirm', message='Passwords must match')])
     confirm = PasswordField('Repeat Password')
     accept_tos = BooleanField('I accept the Terms of Service and Privacy Notice (updated Jan 22, 2015)', [validators.DataRequired()])
